4a02f81e29f07b9c985cd02b228e73d894a4e521
samuelarbibe <samuel@arbibe.org>
2025-04-28 18:09:21 +0300

    add liked songs alert

--- components/steps/playlist-selection-step.tsx
+++ components/steps/playlist-selection-step.tsx
@@ -5,6 +5,8 @@ import { useAtom } from "jotai/react"
 import { Button } from "../ui/button"
 import { IStepProps } from "@/app/page"
 import PlaylistsList from "../services/playlists-list"
+import { Alert, AlertDescription } from "../ui/alert"
+import { AlertCircle } from "lucide-react"
 
 export default function PlaylistSelectionStep(props: IStepProps) {
   const [sourcePlaylistsIds] = useAtom(sourcePlaylistsIdsAtom)
@@ -12,8 +14,13 @@ export default function PlaylistSelectionStep(props: IStepProps) {
   return (
     <>
       <div className="w-full flex flex-col gap-4">
-        <span className="text-lg">Select the playlists you want to transfer</span>
-
+        <Alert>
+          <AlertCircle className="h-4 w-4" />
+          <AlertDescription>
+            If you want to transfer your liked songs, create a playlist and transfer it.
+          </AlertDescription>
+        </Alert>
+        <span className="text-lg">Select the playlists you want to transfer:</span>
         <PlaylistsList serviceId={props.sourceServiceId} />
       </div>
       <div className="flex flex-row w-full items-center justify-center gap-3">
